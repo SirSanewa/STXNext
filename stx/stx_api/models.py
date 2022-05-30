f15d7b92e05f6797061b7c12c98fdc173595dc1d
SirSanewa <lukaszsanewski@gmail.com>
2022-05-30 20:13:47 +0200

    get_with_query_params

--- stx/stx_api/models.py
+++ stx/stx_api/models.py
@@ -5,14 +5,13 @@ class Book(models.Model):
     external_id = models.IntegerField(null=True)
     title = models.CharField(max_length=400)
     authors = models.ManyToManyField("Author")
-    published_year = models.CharField(max_length=4)
+    published_year = models.IntegerField()
     acquired = models.BooleanField()
     thumbnail = models.CharField(null=True, max_length=600)
 
     class Meta:
-        unique_together = ("title", "published_year", )
+        unique_together = ("title", "published_year",)
 
 
 class Author(models.Model):
     fullname = models.CharField(max_length=100)
-
--- stx/stx_api/views.py
+++ stx/stx_api/views.py
@@ -1,15 +1,15 @@
+from rest_framework.exceptions import ValidationError
 from rest_framework.views import APIView
 from rest_framework.views import Response
 from rest_framework import status
 from .serializers import BookReadSerializer, BookWriteSerializer
-from .models import Book
-
+from .models import Book, Author
 
 API_VERSION_DATE = "2022.05.16"
 
 
 class ApiSpec(APIView):
-    def get(self, response):
+    def get(self, request):
         return Response(
             {
                 "info": {
@@ -30,11 +30,39 @@ class Books(APIView):
         return Response(serializer_write.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def get(self, request, book_id=None):
+        if request.query_params:
+            # author podaje w query_param jako str("Autor") wiec trzeba sie pozbyc cudzysłowi
+            author = request.query_params.get("author")
+            stripped_author = author.strip('"')
+            author_details = Author.objects.filter(fullname=stripped_author)
+            # te autor_id wyglada okropnie ale nie moge jakos wyciagnac z queryset id
+            author_id = author_details.values()[0]["id"]
+
+            # tu chyba może być
+            date_from = int(request.query_params.get("from"))
+            date_to = int(request.query_params.get("to"))
+
+            # to jest mega łopatologiczne ale nie wiem jak elegancko zamienić "false" na False
+            acquired = request.query_params.get("acquired")
+            if acquired == "false":
+                bool_acquired = False
+            elif acquired == "true":
+                bool_acquired = True
+            else:
+                raise ValidationError("Parameter 'acquired' can be only false or true")
+
+            # filtrowanie działa zgodnie z założeniem, i wypluwa wszystko co pasuje, wiec tu ok
+            queryset = Book.objects.filter(acquired=bool_acquired) \
+                .filter(published_year__gte=date_from, published_year__lte=date_to)\
+                .filter(authors__in=[author_id])
+            serializer = BookReadSerializer(queryset, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
         if book_id:
             book = Book.objects.get(id=book_id)
             serializer = BookReadSerializer(book)
+            return Response(serializer.data, status=status.HTTP_200_OK)
         else:
             books = Book.objects.all()
             serializer = BookReadSerializer(books, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
+            return Response(serializer.data, status=status.HTTP_200_OK)
