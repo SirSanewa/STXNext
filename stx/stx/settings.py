5a1a735095b55c5a497b9832b26cdba5f014c564
SirSanewa <lukaszsanewski@gmail.com>
2022-06-01 21:45:02 +0200

    database

--- stx/stx/settings.py
+++ stx/stx/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
 from pathlib import Path
+
+import dj_database_url
 from dotenv import load_dotenv, find_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
