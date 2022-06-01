9ef0b6fef4310201416a386d5eba2d4cf95891b3
SirSanewa <lukaszsanewski@gmail.com>
2022-06-01 21:44:03 +0200

    database

--- stx/stx/settings.py
+++ stx/stx/settings.py
@@ -88,7 +88,9 @@ DATABASES = {
         'PORT': os.environ["POSTGRES_PORT"]
     }
 }
-
+DATABASE_URL = os.environ["DATABASE_URL"]
+db_from_env = dj_database_url.config(default=DATABASE_URL, conn_max_age=500, ssl_require=True)
+DATABASES["default"].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
